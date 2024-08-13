541123127f4753d5e80860f32817905566cf44a9
Edson Morro Nogueira <125297431+EdsonMorroNogueira@users.noreply.github.com>
2024-08-13 15:13:20 -0300

    fex(manifest): Fiz do Zero

--- POC-CAPACITOR/vite.config.ts
+++ POC-CAPACITOR/vite.config.ts
@@ -5,9 +5,26 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), 
-    VitePWA({ registerType: 'autoUpdate',
+    VitePWA({
+      includeAssets: ['src/assets/CompressJPEG.online_512x512_image.png', 'src/assets/cropped.jpg'],
       manifest: {
-        theme_color: '#ffc0cb'
+        name: "poc-pwa",
+        short_name:"pocPWA",
+        description: "Poc para transformação de um web app feito em React & Tailwind",
+        theme_color: '#ffc0cb',
+        icons: [
+          {
+            src: 'src/assets/cropped.jpg',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'src/assets/CompressJPEG.online_512x512_image.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
       }
-    })],
+    })
+  ],
 })
